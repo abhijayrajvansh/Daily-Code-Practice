5337bfba34a21279dd5db6f982e36fad0e6c84b1
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-02-13 09:58:48 +0530

    update: gameOver approach 1

--- multidimensionallist/app/components/GameBoard.jsx
+++ multidimensionallist/app/components/GameBoard.jsx
@@ -12,12 +12,19 @@ export default function Board () {
 
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
   const [playerTurn, setPlayerTurn] = useState('X');
-  
+
+  function gameOver () {
+    if (gameBoard[0][0] &&  gameBoard[0][0] === gameBoard[0][1] && gameBoard[0][1] === gameBoard[0][2]) {
+      return true;
+    }
+    return false;
+  }
 
   function handleBoxClick (rowIndex, colIndex) {
+    if (gameBoard[rowIndex][colIndex]) return; // prevents player from double clicking and rechanging value of the box
     // changes player turn after every click
-    setPlayerTurn((prevTurn) => prevTurn === 'X' ? 'O' : 'X')
-    
+    setPlayerTurn((prevTurn) => prevTurn === 'X' ? 'O' : 'X'); 
+  
     // updates the value in the board
     setGameBoard((prevBoard) => {
       const updatedBoard = [...prevBoard.map(innerArray => [...innerArray])];
@@ -26,6 +33,19 @@ export default function Board () {
     });
   }
 
+  if(gameOver()) {
+    return (
+        <div className='gameBoard'>
+          {gameBoard.map((row, rowIndex) => <div key={rowIndex}>
+          <div className='rowBox'>{row.map((col, colIndex) => <div key={colIndex}>
+            <div className='gameBox'>{col}</div>
+          </div>)}</div>
+        </div>)}
+        <h1>Game Over</h1>
+      </div>
+    )
+  }
+  
   return (
     <div className='gameBoard'>
         {gameBoard.map((row, rowIndex) => <div key={rowIndex}>
