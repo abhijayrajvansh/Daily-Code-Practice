0befd884d2b0660b40733b87478b78f4a046b68a
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-02-14 04:04:05 +0530

    update: winnig logic added

--- multidimensionallist/app/components/GameBoard.jsx
+++ multidimensionallist/app/components/GameBoard.jsx
@@ -1,6 +1,8 @@
 'use client'
 
 import {useState} from 'react';
+import {winningLogic}  from './winningLogic';
+import { Baloo_2 } from 'next/font/google';
 
 const initialGameBoard = [
   [null, null, null],
@@ -13,46 +15,49 @@ export default function Board () {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
   const [playerTurn, setPlayerTurn] = useState('X');
 
-  function gameOver () {
-    if (gameBoard[0][0] &&  gameBoard[0][0] === gameBoard[0][1] && gameBoard[0][1] === gameBoard[0][2]) {
-      return true;
-    }
-    return false;
+  let gameOver = false;
+
+  let box1, box2, box3, s1, s2,s3, s4, s5, s6;
+  for (let i = 0; i < winningLogic.length; i++) {
+    s1 = winningLogic[i][0].row;
+    s2 = winningLogic[i][0].col;
+
+    s3 = winningLogic[i][1].row;
+    s4 = winningLogic[i][1].col;
+
+    s5 = winningLogic[i][2].row;
+    s6 = winningLogic[i][2].col;
+
+    box1 = gameBoard[s1][s2];
+    box2 = gameBoard[s3][s4];
+    box3 = gameBoard[s5][s6];
+
+    if (box1 && box1 === box2 && box2 === box3) gameOver = true;
   }
 
+
   function handleBoxClick (rowIndex, colIndex) {
-    if (gameBoard[rowIndex][colIndex]) return; // prevents player from double clicking and rechanging value of the box
+    if (gameBoard[rowIndex][colIndex] || gameOver) return; // prevents player from double clicking and rechanging value of the box
     // changes player turn after every click
     setPlayerTurn((prevTurn) => prevTurn === 'X' ? 'O' : 'X'); 
   
-    // updates the value in the board
+    // updates the playerturn/input in the board
     setGameBoard((prevBoard) => {
       const updatedBoard = [...prevBoard.map(innerArray => [...innerArray])];
       updatedBoard[rowIndex][colIndex] = playerTurn;
       return updatedBoard;
     });
-  }
+  } 
 
-  if(gameOver()) {
-    return (
-        <div className='gameBoard'>
-          {gameBoard.map((row, rowIndex) => <div key={rowIndex}>
-          <div className='rowBox'>{row.map((col, colIndex) => <div key={colIndex}>
-            <div className='gameBox'>{col}</div>
-          </div>)}</div>
-        </div>)}
-        <h1>Game Over</h1>
-      </div>
-    )
-  }
-  
   return (
     <div className='gameBoard'>
         {gameBoard.map((row, rowIndex) => <div key={rowIndex}>
         <div className='rowBox'>{row.map((col, colIndex) => <div key={colIndex}>
-          <div className='gameBox' onClick={() => handleBoxClick (rowIndex, colIndex)}>{col}</div>
-        </div>)}</div>
+          <div className='gameBox' onClick={() => handleBoxClick(rowIndex, colIndex)}>{col}</div>
+        </div>)}
+        </div>
       </div>)}
+      {gameOver && <h1>Game Over!</h1>}
     </div>
   )
 }
