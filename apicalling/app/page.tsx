d3598d1c2cb661ac86d7ffe2cc94ea9d667928a3
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-02-23 19:40:34 +0530

    update: reactStrictMode: false && .env

--- apicalling/app/page.tsx
+++ apicalling/app/page.tsx
@@ -1,5 +1,13 @@
+'use client'
+
 export default function page () {
-  return (
-    <div>api calling</div>
-  )
+
+  const LOCAL_API = process.env.APIKEY
+
+  console.log(LOCAL_API)
+
+  return <section>
+    <h1>API Calling</h1>
+    
+  </section>
 }
