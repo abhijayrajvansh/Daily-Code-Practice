0bf8acc6c1654974dc3a179bae78a25c2c0af114
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-03-22 08:31:49 +0530

    update: audit push from local env

---
+++ css-positions/app/page.tsx
@@ -0,0 +1,24 @@
+const page = () => {
+  return (
+    <div className="wrapper">
+      <h1 className="heading">CSS Positioning!</h1>
+
+
+
+      <div className="border-2 border-red-500 rounded-xl min-h-32z">
+        <div className="byellow flex">
+
+          <img className="h-14 rounded-xl" src="https://play-lh.googleusercontent.com/b79jRmQK2sZABaN0xI2wIAFrEcBxs0CuDzNTmF088DGrJXbemaMNRvrhqH0St28J7CU" alt="companyLogo" />
+
+          <div>
+            <h1 className="text-xl font-medium">Position of Resposiblity</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non sinl Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, voluptatibus tenetur! Modi voluptate eveniet ipsum iusto sint blanditiis tempore corporis quo. Lorem ipsum dolor sit amet consectetur adipisicing</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default page;
